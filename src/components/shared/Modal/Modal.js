ff13eb20d93824d7f9194c42e8699b07028f9d82
NatalieUSA <110269066+NatalieUSA@users.noreply.github.com>
2023-03-10 21:54:06 +0200

    Update Modal.js

--- src/components/shared/Modal/Modal.js
+++ src/components/shared/Modal/Modal.js
@@ -1,4 +1,4 @@
-import { useEffect, useCallback } from 'react';
+import { useEffect } from 'react';
 import { createPortal } from 'react-dom';
 
 import { Close, Overlay, ModalItem } from './Modal.styled';
@@ -6,23 +6,33 @@ import { Close, Overlay, ModalItem } from './Modal.styled';
 const modalRoot = document.querySelector('#modal-root');
 
 export const Modal = ({ close, children }) => {
-  const closeModal = useCallback(
-    ({ target, currentTarget, code }) => {
-      // console.log('render modal');
-      if (target === currentTarget || code === 'Escape') {
-        close();
-      }
-    },
-    [close]
-  );
-
+  console.log('render modal');
+  // const closeModal = useCallback(
+  //   ({ target, currentTarget, code }) => {
+  //     // console.log('render modal');
+  //     if (target === currentTarget || code === 'Escape') {
+  //       close();
+  //     }
+  //   },
+  //   [close]
+  // );
+  const closeModalByEscape = ({ code }) => {
+    if (code === 'Escape') {
+      close();
+    }
+  };
+  const closeModalByClick = ({ target, currentTarget }) => {
+    if (target === currentTarget) {
+      close();
+    }
+  };
   useEffect(() => {
-    document.addEventListener('keydown', closeModal);
-    return () => document.removeEventListener('keydown', closeModal);
-  }, [closeModal]);
+    document.addEventListener('keydown', closeModalByEscape);
+    return () => document.removeEventListener('keydown', closeModalByEscape);
+  }, []);
 
   return createPortal(
-    <Overlay onClick={closeModal}>
+    <Overlay onClick={closeModalByClick}>
       <ModalItem>
         <Close onClick={close}>X</Close>
         {children}
